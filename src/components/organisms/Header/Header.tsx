97f341f8c4b9be9d1078561d5b43cbc9fe15b44c
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2022-10-26 20:03:43 +0300

    fix header

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -6,12 +6,22 @@ import { telegramService } from "services";
 export const Header = () => {
   const { photoUrl, firstName, lastName } = telegramService.getUserData();
 
-  console.log(telegramService.getUserData())
+  console.log(telegramService.getUserData());
 
   return (
     <header className="header">
-      <Avatar size="large" src={photoUrl} icon={<UserOutlined />} />
-      <Typography.Text strong>{`${firstName} ${lastName}`}</Typography.Text>
+      <div>
+        <div style={{ textAlign: "center" }}>
+          <Avatar
+            size='default'
+            src={photoUrl}
+            icon={<UserOutlined />}
+          />
+        </div>
+        <Typography.Text
+          strong
+        >{`${firstName} asd ${lastName} asd`}</Typography.Text>
+      </div>
     </header>
   );
 };
