b86fffce83dc84163d3e86656bd67b168aedb89e
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2022-10-26 19:22:33 +0300

    debug

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -6,6 +6,8 @@ import { telegramService } from "services";
 export const Header = () => {
   const { photoUrl, firstName, lastName } = telegramService.getUserData();
 
+  console.log(telegramService.getUserData())
+
   return (
     <header className="header">
       <Avatar size="large" src={photoUrl} icon={<UserOutlined />} />
