3848670d16f93ea58c440fb95b9e776094ca0b49
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2022-10-26 21:38:56 +0300

    delete footer

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,11 @@
-import { Footer, Header } from "components/organisms";
+import { Header } from "components/organisms";
 import { CatalogPage, FormPage } from "components/pages";
 import { BaseTemplate } from "components/templates";
 import { Route, Routes } from "react-router-dom";
 
 export const App = () => {
   return (
-    <BaseTemplate header={<Header />} footer={<Footer />}>
+    <BaseTemplate header={<Header />}>
       <Routes>
         <Route path="/catalog" element={<CatalogPage />} />
         <Route path="/form" element={<FormPage />} />
--- src/components/templates/BaseTemplate/BaseTemplate.tsx
+++ src/components/templates/BaseTemplate/BaseTemplate.tsx
@@ -3,20 +3,14 @@ import "./baseTemplate.css";
 
 interface BaseTemplateProps {
   header: ReactNode;
-  footer: ReactNode;
   children: ReactNode;
 }
 
-export const BaseTemplate: FC<BaseTemplateProps> = ({
-  header,
-  footer,
-  children,
-}) => {
+export const BaseTemplate: FC<BaseTemplateProps> = ({ header, children }) => {
   return (
     <div className="base-template">
       {header}
       <div className="base-template-content">{children}</div>
-      {footer}
     </div>
   );
 };
